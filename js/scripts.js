5029cd0f729acbb78e5659857b1072ab105ce7de
luizpgt <luiz.paulus51@gmail.com>
2023-06-28 15:19:21 -0300

    feat: iniciando logica do quizz

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,96 @@
-console.log("projeto bomba atomica!");
+// declaracao variaveis
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box"); // alternativas
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestion = 0;
+
+// Perguntas
+const questions = [
+  {
+    "question": "PHP foi desenvolvido para qual fim?",
+    "answers": [
+      {
+        "answer": "back-end",
+        "correct": true
+      },
+      {
+        "answer": "front-end",
+        "correct": false
+      },
+      {
+        "answer": "Sistema operacional",
+        "correct": false
+      },
+      {
+        "answer": "Banco de dados",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Uma forma de declarar variável em JavaScript:",
+    "answers": [
+      {
+        "answer": "$var",
+        "correct": false
+      },
+      {
+        "answer": "var",
+        "correct": true
+      },
+      {
+        "answer": "@var",
+        "correct": false
+      },
+      {
+        "answer": "#let",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Qual o seletor de id no CSS?",
+    "answers": [
+      {
+        "answer": "#",
+        "correct": true
+      },
+      {
+        "answer": ".",
+        "correct": false
+      },
+      {
+        "answer": "@",
+        "correct": false
+      },
+      {
+        "answer": "/",
+        "correct": false
+      },
+    ]
+  },
+]
+
+// substituicao do quizz para a primeria pergunta
+function init () {
+  createQuestion(0);
+}
+
+function createQuestion (i) {
+  // limpar a questão anterior | remove botões da questão anterior 
+  const oldButton = answersBox.querySelectorAll("button");
+  oldButton.forEach(function(btn) { btn.remove();});
+
+  // alterar texto da pergunta
+  const questionText = question.querySelector("#question-text");
+  const questionNumber = question.querySelector("#question-number");
+
+  questionText.textContent = questions[i].question;
+  questionNumber.textContent = i + 1;
+
+}
+// inicializacao do quizz
+init();
